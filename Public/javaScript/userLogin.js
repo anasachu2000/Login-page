b222117f980e651aad566f544f5aff990cc1ad6a
anasachu2000 <125673735+anasachu2000@users.noreply.github.com>
2023-04-25 20:45:00 +0530

    first commit

--- Public/javaScript/userLogin.js
+++ Public/javaScript/userLogin.js
@@ -1,36 +1,35 @@
 
-const form = document.getElementById("form");
-const usernameInput = document.getElementById("username");
-const passwordInput = document.getElementById("password");
+  const form = document.getElementById("form");
+  const usernameInput = document.getElementById("username");
+  const passwordInput = document.getElementById("password");
 
-const usernameError = document.getElementById("username-error");
-const passwordError = document.getElementById("password-error");
+  const usernameError = document.getElementById("username-error");
+  const passwordError = document.getElementById("password-error");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  const usernameValue = usernameInput.value.trim();
-  const passwordValue = passwordInput.value.trim();
+    const usernameValue = usernameInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
 
-  if (!usernameValue) {
-    usernameError.textContent = "Please enter a username";
-    return;
-  } else {
-    usernameError.textContent = "";
-  }
+    if (!usernameValue) {
+      usernameError.textContent = "Please enter a username";
+      return;
+    } else {
+      usernameError.textContent = "";
+    }
 
-  if (!passwordValue) {
-    passwordError.textContent = "Please enter a password";
-    return;
-  } else {
-    passwordError.textContent = "";
-  }
-  if (usernameValue !== "anas" || passwordValue !== "1234") {
-    // usernameError.textContent = 'Username and password do not match';
-    passwordError.textContent = "Username and password do not match";
-    return;
-  }
+    if (!passwordValue) {
+      passwordError.textContent = "Please enter a password";
+      return;
+    } else {
+      passwordError.textContent = "";
+    }
+    if (usernameValue !== "anas" || passwordValue !== "1234") {
+      // usernameError.textContent = 'Username and password do not match';
+      passwordError.textContent = "Username and password do not match";
+      return;
+    }
 
-  form.submit();
-});
- 
+    form.submit();
+  });
--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express()
 const session = require('express-session')
-const bodyParser = require('body-parser'); 
 const nocache = require('nocache');  
 
-// app.use(express.static('public')) 
+ app.use(express.static('public')) 
 
 app.use(nocache());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
     session({
       secret: "key123",
